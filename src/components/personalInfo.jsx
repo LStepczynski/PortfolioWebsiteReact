8d5edc2429a098b544ba3fa3c9e9b4c12b05846f
LStepczynski <leonstepczynski@gmail.com>
2024-03-28 18:19:25 -0500

    feat: added new animations

--- src/components/personalInfo.jsx
+++ src/components/personalInfo.jsx
@@ -1,10 +1,37 @@
-import { Box, Heading } from '@primer/react'
+import { Box, Heading, Text } from '@primer/react'
 import { useState, useEffect, useRef } from 'react'
 import anime from 'animejs';
+
+const containerStyle = {
+  fontFamily: "M PLUS Code Latin, sans-serif",
+  transition: "transform 1s ease",
+  transform: "translateX(-100px)",
+  display: "grid",
+  width: "60%",
+  opacity: 0,
+  '@media screen and (max-width: 1012px)': {
+    width: "75%",
+  },
+  '@media screen and (max-width: 768px)': {
+    width: "80%",
+  },
+}
 
 export const PersonalInformation = ({ children, title, id}) => {
   const [hovering, setHovering] = useState(false)
   const textRef = useRef(null);
+  const borderRef = useRef(null);
+
+  const isAnimating = (targetElement) => {
+    // Iterate over the running animations
+    for (const animation of anime.running) {
+      // Check if the animation is targeting the specified element
+      if (animation.animatables.some(animatable => animatable.target === targetElement)) {
+        return true; // Animation is running on the target element
+      }
+    }
+    return false; // No animation is running on the target element
+  }
 
   useEffect(() => {
     const options = {
@@ -35,12 +62,12 @@ export const PersonalInformation = ({ children, title, id}) => {
 
     // Create an intersection observer
     const observer = new IntersectionObserver(handleIntersection, options);
-
+    
     // Observe the image element
     if (textRef.current) {
       observer.observe(textRef.current);
     }
-
+    
     // Cleanup observer on component unmount
     return () => {
       if (textRef.current) {
@@ -48,52 +75,84 @@ export const PersonalInformation = ({ children, title, id}) => {
       }
     };
   }, []);
+  
+  const borderAnimationEntry = {
+    targets: borderRef.current,
+    delay: 100,
+    width: "100%",
+    duration: 500,
+    easing: 'easeOutQuart',
+    complete: () => {
+      borderRef.current.style.justifySelf = "right"
+      anime(borderAnimationLeave)
+    },
+  }
+  
+  const borderAnimationLeave = {
+    targets: borderRef.current,
+    width: "0%",
+    duration: 400,
+    delay: 300,
+    easing: 'easeInSine',
+    complete: () => {
+      borderRef.current.style.justifySelf = "left"
+    }
+  }
 
+  useEffect(() => {
+    // console.log(isAnimating(borderRef.current))
+    if (!isAnimating(borderRef.current) && hovering) {
+      anime(borderAnimationEntry)
+    }
+
+  }, [hovering])
+
+  
   return (
     <Box 
-      sx={{
-        display: "grid", 
-        justifyItems: "center", 
-        mt: "150px"
-      }}
+    sx={{
+      display: "grid", 
+      justifyItems: "center", 
+      mt: "150px"
+    }}
     >
       <Box
         id={id}
         ref={textRef}
-        onMouseOver={() => {setHovering(true)}}
-        onMouseLeave={() => {setHovering(false)}}
-        sx={{
-          width: "60%",
-          transition: "transform 1s ease",
-          opacity: 0,
-          transform: "translateX(-100px)",
-          fontFamily: "M PLUS Code Latin, sans-serif",
-          justifyContent: "space-around",
-          '@media screen and (max-width: 1012px)': {
-            width: "75%",
-          },
-          '@media screen and (max-width: 768px)': {
-            width: "80%",
-          },
-        }}
+        onMouseOver={() => setHovering(true)}
+        onMouseLeave={() => setHovering(false)}
+        sx={containerStyle}
       >
         <Heading
           sx={{
             transition: "all 0.3s ease",
-            letterSpacing: hovering ? "5px" : "",
+            color: hovering ? "" : "ansi.white",
+            letterSpacing: hovering ? "1px" : "",
             transform: hovering ? "translateY(5px)" : "",
+            py: 2,
           }}
         >
           {title}
         </Heading>
-        <p 
-          style={{
+        <Box
+          ref={borderRef}
+          sx={{
+            backgroundColor: "white",
+            height: "1px",
+            width: "0%",
+            justifySelf: "left",
+          }}
+        ></Box>
+        <Text as="p" 
+          sx={{
             textAlign: "justify",
             fontSize: "20px",
+            py: 2,
+            m: 0,
           }}
         >
           {children}
-        </p>
+        </Text>
       </Box>
     </Box>
   )
