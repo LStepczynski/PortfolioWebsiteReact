4adb2d0b97b24bf0d166704aa89832a0b19a2863
LStepczynski <leonstepczynski@gmail.com>
2024-04-04 20:35:15 -0500

    feat: created a file that will hold all the image paths

---
+++ src/components/animation/icons.js
@@ -0,0 +1,12 @@
+
+
+export const images = {
+    react: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png",
+    javascript: "https://static-00.iconduck.com/assets.00/javascript-js-icon-2048x2048-nyxvtvk0.png",
+    html: "https://cdn-icons-png.flaticon.com/512/732/732212.png",
+    css: "https://cdn.worldvectorlogo.com/logos/css-3.svg",
+    django: "https://www.svgrepo.com/show/353657/django-icon.svg",
+    python: "https://cdn.iconscout.com/icon/free/png-256/free-python-3521655-2945099.png",
+    nodejs: "https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png",
+    github: "https://www.svgrepo.com/show/35001/github.svg",
+}
