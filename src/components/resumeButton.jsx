2cafbbd6582ade1564a378831ee8967cced88044
LStepczynski <leonstepczynski@gmail.com>
2024-04-07 20:30:11 -0500

    feat: added the option to download the resume

--- src/components/resumeButton.jsx
+++ src/components/resumeButton.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-import { Box, Text } from '@primer/react'
+import { Box, Text, Link } from '@primer/react'
 import { MoveToBottomIcon } from '@primer/octicons-react';
 
 export const ResumeButton = () => {
@@ -20,50 +20,61 @@ export const ResumeButton = () => {
           mb: 12,
         }}
       >
-        <Box
-          onClick={handleClick}
-          onMouseEnter={() => { setHover(true) }}
-          onMouseLeave={() => { setHover(false) }}
+        <Link 
+          target="_blank" 
+          href="Resume-LS.pdf"
           sx={{
-            backgroundColor: hover ? "accent.fg" : "accent.emphasis",
-            fontFamily: "M PLUS Code Latin, sans-serif",
-            cursor: "pointer",
-            transition: "background-color 0.2s",
-            borderRadius: "10px",
-            fontSize: "28px",
-            mt: "40px",
-            px: 4,
-            py: 2,
+            color: "white",
             ":hover": {
-              backgroundColor: "accent.fg",
+              textDecoration: "none",
             }
           }}
         >
           <Box
+            onClick={handleClick}
+            onMouseEnter={() => { setHover(true) }}
+            onMouseLeave={() => { setHover(false) }}
             sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 3
+              backgroundColor: hover ? "accent.fg" : "accent.emphasis",
+              fontFamily: "M PLUS Code Latin, sans-serif",
+              cursor: "pointer",
+              transition: "background-color 0.2s",
+              borderRadius: "10px",
+              fontSize: "28px",
+              mt: "40px",
+              px: 4,
+              py: 2,
+              ":hover": {
+                backgroundColor: "accent.fg",
+              }
             }}
           >
-            <Box>
-              <Text
-                sx={{
-                  fontSize: "30px",
-                  '@media screen and (max-width: 768px)': {
-                    fontSize: "22px"
-                  },
-                }}
-              >
-                Download Resume
-              </Text>
-            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                alignItems: "center",
+                gap: 3
+              }}
+            >
+              <Box>
+                <Text
+                  sx={{
+                    fontSize: "30px",
+                    '@media screen and (max-width: 768px)': {
+                      fontSize: "22px"
+                    },
+                  }}
+                >
+                  Download Resume
+                </Text>
+              </Box>
 
-            <Box sx={{ display: "flex", alignItems: "center" }}>
-              <MoveToBottomIcon size={40} />
+              <Box sx={{ display: "flex", alignItems: "center" }}>
+                <MoveToBottomIcon size={40} />
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </Link>
       </Box>
     </>
   )
